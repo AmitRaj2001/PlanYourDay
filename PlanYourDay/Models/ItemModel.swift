3fb12c4c1a2739124bb83a72b4ee9dacf7f21098
AmitRaj2001 <amit.12018323@lpu.in>
2023-08-17 23:22:31 +0530

    modified:   PlanYourDay.xcodeproj/project.pbxproj 	deleted:    PlanYourDay/ContentView.swift 	new file:   PlanYourDay/Models/ItemModel.swift 	modified:   PlanYourDay/PlanYourDayApp.swift 	new file:   PlanYourDay/ViewModels/ListViewModel.swift 	new file:   PlanYourDay/Views/AddView.swift 	new file:   PlanYourDay/Views/ListRowView.swift 	new file:   PlanYourDay/Views/ListView.swift 	new file:   PlanYourDay/Views/NoItemsView.swift

---
+++ PlanYourDay/Models/ItemModel.swift
@@ -0,0 +1,27 @@
+//
+//  ItemModel.swift
+//  PlanYourDay
+//
+//  Created by AMIT RAJ on 17/08/23.
+//
+
+
+import Foundation
+
+// Immutable Struct has only 'let' constants
+struct ItemModel: Identifiable, Codable {
+    let id: String
+    let title: String
+    let isCompleted: Bool
+    
+    init(id: String = UUID().uuidString, title: String, isCompleted: Bool) {
+        self.id = id
+        self.title = title
+        self.isCompleted = isCompleted
+    }
+    
+    func updateCompletion() -> ItemModel {
+        return ItemModel(id: id, title: title, isCompleted: !isCompleted)
+    }
+    
+}
---
+++ PlanYourDay/Views/ListRowView.swift
@@ -0,0 +1,40 @@
+//
+//  ListRowView.swift
+//  PlanYourDay
+//
+//  Created by AMIT RAJ on 17/08/23.
+//
+
+
+import SwiftUI
+
+struct ListRowView: View {
+    
+    let item: ItemModel
+    
+    var body: some View {
+        HStack {
+            Image(systemName: item.isCompleted ? "checkmark.circle" : "circle")
+                .foregroundColor(item.isCompleted ? .green : .red)
+            Text(item.title)
+            Spacer()
+        }
+        .font(.title2)
+        .padding(.vertical, 8)
+    }
+}
+
+struct ListRowView_Previews: PreviewProvider {
+    
+    static var item1 = ItemModel(title: "First item!", isCompleted: false)
+    static var item2 = ItemModel(title: "Second Item.", isCompleted: true)
+    
+    static var previews: some View {
+        Group {
+            ListRowView(item: item1)
+            ListRowView(item: item2)
+        }
+        .previewLayout(.sizeThatFits)
+        
+    }
+}

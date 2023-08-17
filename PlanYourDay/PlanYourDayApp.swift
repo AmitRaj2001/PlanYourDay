5a89a10e032e7080abd202cb4616cd36a8812036
AmitRaj2001 <amit.12018323@lpu.in>
2023-08-17 22:01:03 +0530

    Initial Commit

---
+++ PlanYourDay/PlanYourDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanYourDayApp.swift
+//  PlanYourDay
+//
+//  Created by SUMIT RAJ on 17/08/23.
+//
+
+import SwiftUI
+
+@main
+struct PlanYourDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
